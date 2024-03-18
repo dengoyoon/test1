37ffbc7e9b64516acc1a30772fc35e8780d01ac4
dengoyoon <duhyeoon@gmail.com>
2024-03-19 00:03:44 +0900

    commit 2

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { setupCounter } from "./counter.ts";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    <h1 class="text-3xl font-bold underline">Hello World</h1>
+    <h1 class="text-3xl font-bold">Hello World</h1>
   </div>
 `;
 
